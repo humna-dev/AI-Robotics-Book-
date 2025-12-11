47ab858a749a83775e22a8459ed6e741e0024d9d
Humna <hamnaallauddin4@gmail.com>
2025-12-11 13:56:55 +0500

    update project

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://your-username.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/physical-ai-humanoid-robotics-textbook',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'your-username', // Usually your GitHub org/user name.
